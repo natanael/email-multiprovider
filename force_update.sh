67847c11acbda86eefb04f8367ec091914a2d8ae
natanael <natanaelguerra@gmail.com>
2019-12-01 16:40:51 -0300

    Fixes to docker-compose and force_update

--- force_update.sh
+++ force_update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -x
 git pull
 rm -rf dist
 yarn build
 docker-compose -f docker-compose.yml build
 docker-compose -f docker-compose.yml down
-docker-compose -f docker-compose.yml up
+docker-compose -f docker-compose.yml up -d
